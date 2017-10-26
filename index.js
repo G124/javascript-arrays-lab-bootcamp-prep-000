c6a0b03c1341f31e4a257982294f81c86db3ad03
Gavriel Hass <gavhass@gmail.com>
2017-10-26 15:32:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 const app = "I don't do much."
+
+
+function  destructivelyAppendKitten(name) {
+
+ kittens.push(name)
+
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+
+}
+
+function destructivelyRemoveFirstKitten() {
+
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  ["kitten",...kittens]
+
+}
